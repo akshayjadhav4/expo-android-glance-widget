e23c4c635eaca925f22979db446884b91740cf5a
Akshay Jadhav <akshayjadhav461999@gmail.com>
2025-06-09 20:58:24 +0530

    Remove View module boilerplate files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // Reexport the native module. On web, it will be resolved to ExpoAndroidGlanceWidgetModule.web.ts
 // and on native platforms to ExpoAndroidGlanceWidgetModule.ts
-export { default } from './ExpoAndroidGlanceWidgetModule';
-export { default as ExpoAndroidGlanceWidgetView } from './ExpoAndroidGlanceWidgetView';
-export * from  './ExpoAndroidGlanceWidget.types';
+export { default } from "./ExpoAndroidGlanceWidgetModule";
+export * from "./ExpoAndroidGlanceWidget.types";
